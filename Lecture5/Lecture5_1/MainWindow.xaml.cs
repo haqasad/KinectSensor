f84a904a8a06c7ec843b82e165021712c3f51aaa
haqasad <haq.asad000@gmail.com>
2016-06-11 16:08:14 -0400

    Modified Lecture 5_1

--- Lecture5/Lecture5_1/MainWindow.xaml.cs
+++ Lecture5/Lecture5_1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Kinect;
+//using Lecture4_1;
 
 namespace Lecture5_1
 {
@@ -23,6 +27,171 @@ namespace Lecture5_1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        KinectSensor sensor;
+        private WriteableBitmap colorBitmap;
+        private byte[] colorPixels;
+
+        private int TotalFrames;
+        private DateTime lastTime = DateTime.MaxValue;
+        private int LastFrames;
+        int currentFrameRate = 0;
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private int GetCurrentFrameRate()
+        {
+            ++this.TotalFrames;
+            DateTime currentTime = DateTime.Now;
+            var timeSpan = currentTime.Subtract(this.lastTime);
+            if(this.lastTime == DateTime.MaxValue || timeSpan >= TimeSpan.FromSeconds(1))
+            {
+                currentFrameRate = (int)Math.Round((this.TotalFrames - this.LastFrames) / timeSpan.TotalSeconds);
+                this.LastFrames = this.TotalFrames;
+                this.lastTime = currentTime;
+            }
+            return currentFrameRate;
+        }
+
+        private void SaveImage(object sender, RoutedEventArgs e)
+        {
+            SaveImage();
+        }
+
+        private void SaveImage()
+        {
+            using (FileStream fileStream = new FileStream(string.Format("{0}.jpg", Guid.NewGuid().ToString()),System.IO.FileMode.Create)){
+                BitmapSource imageSource = (BitmapSource)image1.Source;
+                JpegBitmapEncoder jpegEncoder = new JpegBitmapEncoder();
+                jpegEncoder.Frames.Add(BitmapFrame.Create(imageSource));
+                jpegEncoder.Save(fileStream);
+                fileStream.Close();
+            }
+        }
+
+        public void StartTimer()
+        {
+            this.timer.Interval = new TimeSpan(0, 0, 10);
+            this.timer.Start();
+            this.timer.Tick += handleTickEvent;
+        }
+
+        //private DispatcherTimer timer = new DispatcherTimer();
+
+        public void handleTickEvent(object sender, object e)
+        {
+            if(this.sensor.IsRunning && this.sensor.ColorStream.IsEnabled)
+            {
+                this.SaveImage();
+            }
+        }
+
+        //timer.StartTimer();
+
+        public void StopTimer()
+        {
+            this.timer.Stop();
+            this.timer.Tick -= this.handleTickEvent;
+        }
+
+        private void SetSensorAngle(int angleValue)
+        {
+            if (angleValue > sensor.MinElevationAngle || angleValue < sensor.MaxElevationAngle)
+            {
+                this.sensor.ElevationAngle = angleValue;
+            }
+        }
+
+        private void IncrementAngle(object sender, RoutedEventArgs e)
+        {
+            int angleValue = this.sensor.ElevationAngle + 1;
+            if (angleValue < sensor.MaxElevationAngle)
+            {
+                this.sensor.ElevationAngle = angleValue;
+                this.AngleBox.Text = "" + angleValue;
+            }
+        }
+
+        private void DecrementAngle(object sender, RoutedEventArgs e)
+        {
+            int angleValue = this.sensor.ElevationAngle - 1;
+            if (angleValue > sensor.MinElevationAngle)
+            {
+                this.sensor.ElevationAngle = angleValue;
+                this.AngleBox.Text = "" + angleValue;
+            }
+        }
+
+        private void ChangeToHighResolution(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                this.normalresolution.IsChecked = false;
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution1280x960Fps12);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+            }
+        }
+
+        private void ChangeToNormalResolution(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                this.highresolution.IsChecked = false;
+                this.sensor.ColorStream.Enable(ColorImageFormat.RawBayerResolution640x480Fps30);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+            }
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+                if(this.sensor!=null && !this.sensor.IsRunning)
+                {
+                    this.sensor.ColorStream.Enable();
+                    this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                    this.image1.Source = this.colorBitmap;
+                    this.sensor.ColorFrameReady += this.colorFrameReady;
+                    this.sensor.Start();
+                }
+                else
+                {
+                    MessageBox.Show("No device is connected!");
+                    this.Close();
+                }
+            }            
+        }
+
+        private void WindowClosing(object sender, ConsoleCancelEventArgs e)
+        {
+            if(this.sensor!=null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        void colorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
+            {
+                if (null == imageFrame)
+                    return;
+                int stride = imageFrame.Width * imageFrame.BytesPerPixel;
+
+                this.colorBitmap.WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight), this.colorPixels, stride, 0);
+                textBox1.Text = "" + GetCurrentFrameRate();
+            }
+        }
+
+        private void PeriodicSavingChanged(object sender, RoutedEventArgs e)
+        {
+            if (checkBox1.IsChecked == true)
+            {
+                StartTimer();
+            }
+            else { this.timer.Stop(); }
         }
     }
 }
