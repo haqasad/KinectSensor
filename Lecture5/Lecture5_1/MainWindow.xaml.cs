87af306fecfd7c49a86e20f2523fe3f1d230e75d
haqasad <haq.asad000@gmail.com>
2016-06-13 05:35:59 -0400

    Added modification

--- Lecture5/Lecture5_1/MainWindow.xaml.cs
+++ Lecture5/Lecture5_1/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.ComponentModel;
 using Microsoft.Kinect;
 //using Lecture4_1;
 
@@ -38,6 +39,41 @@ namespace Lecture5_1
         private int LastFrames;
         int currentFrameRate = 0;
         private DispatcherTimer timer = new DispatcherTimer();
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+                if (this.sensor != null && !this.sensor.IsRunning)
+                {
+                    //this.sensor.ColorStream.Enable();
+                    //this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                    this.image1.Source = this.colorBitmap;
+                    this.sensor.ColorFrameReady += this.colorFrameReady;                    
+                    this.sensor.Start();
+                }
+                else
+                {
+                    MessageBox.Show("No device is connected!");
+                    this.Close();
+                }
+            }
+        }
+
+        void colorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
+            {
+                if (null == imageFrame)
+                    return;
+                int stride = imageFrame.Width * imageFrame.BytesPerPixel;
+
+                this.colorBitmap.WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight), this.colorPixels, stride, 0);
+                textBox1.Text = "" + GetCurrentFrameRate();
+            }
+        }
 
         private int GetCurrentFrameRate()
         {
@@ -76,7 +112,11 @@ namespace Lecture5_1
             this.timer.Tick += handleTickEvent;
         }
 
-        //private DispatcherTimer timer = new DispatcherTimer();
+        public void StopTimer()
+        {
+            this.timer.Stop();
+            this.timer.Tick -= this.handleTickEvent;
+        }
 
         public void handleTickEvent(object sender, object e)
         {
@@ -85,22 +125,34 @@ namespace Lecture5_1
                 this.SaveImage();
             }
         }
-
-        //timer.StartTimer();
-
-        public void StopTimer()
+                
+        private void ChangeToHighResolution(object sender, RoutedEventArgs e)
         {
-            this.timer.Stop();
-            this.timer.Tick -= this.handleTickEvent;
-        }
-
-        private void SetSensorAngle(int angleValue)
-        {
-            if (angleValue > sensor.MinElevationAngle || angleValue < sensor.MaxElevationAngle)
+            if (this.sensor != null)
             {
-                this.sensor.ElevationAngle = angleValue;
+                this.normalresolution.IsChecked = false;                
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution1280x960Fps12);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
             }
         }
+
+        private void ChangeToNormalResolution(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                this.highresolution.IsChecked = false;                
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+            }
+        }
+
+        //private void SetSensorAngle(int angleValue)
+        //{
+        //    if (angleValue > sensor.MinElevationAngle || angleValue < sensor.MaxElevationAngle)
+        //    {
+        //        this.sensor.ElevationAngle = angleValue;
+        //    }
+        //}
 
         private void IncrementAngle(object sender, RoutedEventArgs e)
         {
@@ -121,70 +173,7 @@ namespace Lecture5_1
                 this.AngleBox.Text = "" + angleValue;
             }
         }
-
-        private void ChangeToHighResolution(object sender, RoutedEventArgs e)
-        {
-            if (this.sensor != null)
-            {
-                this.normalresolution.IsChecked = false;
-                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution1280x960Fps12);
-                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
-            }
-        }
-
-        private void ChangeToNormalResolution(object sender, RoutedEventArgs e)
-        {
-            if (this.sensor != null)
-            {
-                this.highresolution.IsChecked = false;
-                this.sensor.ColorStream.Enable(ColorImageFormat.RawBayerResolution640x480Fps30);
-                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
-            }
-        }
-
-        private void WindowLoaded(object sender, RoutedEventArgs e)
-        {
-            if (KinectSensor.KinectSensors.Count > 0)
-            {
-                this.sensor = KinectSensor.KinectSensors[0];
-                if(this.sensor!=null && !this.sensor.IsRunning)
-                {
-                    this.sensor.ColorStream.Enable();
-                    this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
-                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-                    this.image1.Source = this.colorBitmap;
-                    this.sensor.ColorFrameReady += this.colorFrameReady;
-                    this.sensor.Start();
-                }
-                else
-                {
-                    MessageBox.Show("No device is connected!");
-                    this.Close();
-                }
-            }            
-        }
-
-        private void WindowClosing(object sender, ConsoleCancelEventArgs e)
-        {
-            if(this.sensor!=null && this.sensor.IsRunning)
-            {
-                this.sensor.Stop();
-            }
-        }
-
-        void colorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
-        {
-            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
-            {
-                if (null == imageFrame)
-                    return;
-                int stride = imageFrame.Width * imageFrame.BytesPerPixel;
-
-                this.colorBitmap.WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight), this.colorPixels, stride, 0);
-                textBox1.Text = "" + GetCurrentFrameRate();
-            }
-        }
-
+        
         private void PeriodicSavingChanged(object sender, RoutedEventArgs e)
         {
             if (checkBox1.IsChecked == true)
@@ -193,5 +182,13 @@ namespace Lecture5_1
             }
             else { this.timer.Stop(); }
         }
+
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            if(this.sensor!=null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }       
     }
 }
