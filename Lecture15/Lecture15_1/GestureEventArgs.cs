6f2fd09820e70ebc4af45e93f60f8a334dd15bb6
Asadul Haque <Asadul Haque@LAPTOP_ASAD>
2016-06-29 16:17:36 -0400

    Add Lecture15

---
+++ Lecture15/Lecture15_1/GestureEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture15_1
+{
+    class GestureEventArgs
+    {
+        public GestureRecognitionEngine.GestureType gsType { get; internal set; }
+        public GestureRecognitionEngine.RecognitionResult Result { get; internal set; }
+        public GestureEventArgs(GestureRecognitionEngine.GestureType t, GestureRecognitionEngine.RecognitionResult result)
+        {
+            this.Result = result;
+            this.gsType = t;
+        }
+    }
+}
