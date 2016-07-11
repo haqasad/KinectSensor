82e278cc1db291adb56433d9f3cf7fe20eca1365
haqasad <haq.asad000@gmail.com>
2016-07-11 14:53:00 -0400

    Modified Lecture16

---
+++ Lecture16/Lecture16_1/GestureEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture16_1
+{
+    public class GestureEventArgs : EventArgs
+    {
+        public RecognitionResult Result { get; internal set; }
+        public GestureType GestureType { get; internal set; }
+        public GestureEventArgs(RecognitionResult result, GestureType type)
+        {
+            this.Result = result;
+            this.GestureType = type;
+        }
+    }
+}
---
+++ Lecture16/Lecture16_1/GestureHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace Lecture16_1
+{
+    class GestureHelper
+    {
+        public static float GetJointDistance(Joint firstJoint, Joint secondJoint)
+        {
+            float distanceX = firstJoint.Position.X - secondJoint.Position.X;
+            float distanceY = firstJoint.Position.Y - secondJoint.Position.Y;
+            float distanceZ = firstJoint.Position.Z - secondJoint.Position.Z;
+            return (float)Math.Sqrt(Math.Pow(distanceX, 2) + Math.Pow(distanceY, 2) + Math.Pow(distanceZ, 2));
+        }
+    }
+}
