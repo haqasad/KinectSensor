5a00b50d51e0427cc03ee50a5d1189d87cf2c597
haqasad <haq.asad000@gmail.com>
2016-07-13 16:52:46 -0400

    Modified Lecture12

--- Lecture12/Lecture12_2/MainWindow.xaml.cs
+++ Lecture12/Lecture12_2/MainWindow.xaml.cs
@@ -33,29 +33,54 @@ namespace Lecture12_2
         }
 
         KinectSensor sensor;
+
         Skeleton[] totalSkeleton = new Skeleton[6];
-        Skeleton skeleton;
-        int currentSkeletonID = 0;
-        WriteableBitmap colorBitmap;
-        byte[] colorPixels;
+
+        private WriteableBitmap colorBitmap;
+        private byte[] colorPixels;        
+        
+        public double height = 0;
+        public double width = 0;        
+        
+        int shapeCondition = 0;
+        int positionCondition = 0;
+
+        Color objectColor;
+        Shape drawObject;
+
+        Point righthandPoint = new Point();
+        Point lefthandPoint = new Point();
+
         Stream audioStream;
         SpeechRecognitionEngine speechEngine;
-
+        
         private void WindowLoaded(object sender, RoutedEventArgs e)
-        {
+        {            
+
             this.sensor = KinectSensor.KinectSensors[0];
-            this.sensor.Start();
+            
+            if (this.sensor!=null && !this.sensor.IsRunning)
+            {
+                this.sensor.Start();
+                
+                var smoothParameters = new TransformSmoothParameters
+                {
+                    Correction = 0.1f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.05f,
+                    Prediction = 0.1f,
+                    Smoothing = 0.5f
+                };
+                this.sensor.SkeletonStream.Enable(smoothParameters);
+                this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+                this.sensor.SkeletonFrameReady += skeletonFrameReady;
 
-            this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
-            this.sensor.SkeletonStream.Enable();
-            this.sensor.SkeletonFrameReady += this.skeletonFrameReady;
-
-            this.sensor.ColorStream.Enable();
-            this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
-            this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-
-            //this.image.Source = this.colorBitmap;
-            this.sensor.ColorFrameReady += this.colorFrameReady;
+                this.sensor.ColorStream.Enable();
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                this.image1.Source = this.colorBitmap;
+                this.sensor.ColorFrameReady += this.colorFrameReady;
+            }
 
             audioStream = this.sensor.AudioSource.Start();
             RecognizerInfo recognizerInfo = GetKinectRecognizer();
@@ -67,10 +92,13 @@ namespace Lecture12_2
 
             BuildGrammarforRecognizer(recognizerInfo); // provided earlier
             statusBar.Text = "Speech Recognizer is ready";
-        }
 
+        }
+        
         void colorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
         {
+            //Image image1 = new Image();
+            //Image.HorizontalAlignmentProperty(image1, Left);
             using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
             {
                 if (null == imageFrame)
@@ -79,16 +107,32 @@ namespace Lecture12_2
                 imageFrame.CopyPixelDataTo(colorPixels);
                 int stride = imageFrame.Width * imageFrame.BytesPerPixel;
 
+                // Write the pixel data into our bitmap
                 this.colorBitmap.WritePixels(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight), this.colorPixels, stride, 0);
+                //canvas1.Children.Add(image1);
             }
         }
 
         void skeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            canvas1.Children.Clear();
-            //advanceThingPosition();
-            //canvas1.Children.Add(thing.Shape);
+            canvas1.Children.Remove(drawObject);  
+                      
+            if (shapeCondition == 1)
+            {
+                drawObject = new Ellipse();                
+            }
+            else if (shapeCondition == 2)
+            {
+                drawObject = new Rectangle();                
+            }
 
+            if (drawObject != null)
+            {
+                drawObject.Height = height;
+                drawObject.Width = width;
+                drawObject.Fill = new SolidColorBrush(objectColor);                                
+            } 
+                       
             using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
             {
                 if (skeletonFrame == null)
@@ -96,85 +140,61 @@ namespace Lecture12_2
                     return;
                 }
                 skeletonFrame.CopySkeletonDataTo(totalSkeleton);
-                skeleton = (from trackskeleton in totalSkeleton where trackskeleton.TrackingState == SkeletonTrackingState.Tracked select trackskeleton).FirstOrDefault();
-                if (skeleton == null)
+                Skeleton firstSkeleton = (from trackskeleton in totalSkeleton
+                                          where trackskeleton.TrackingState == SkeletonTrackingState.Tracked
+                                          select trackskeleton).FirstOrDefault();                
+                if (firstSkeleton == null)
                 {
                     return;
                 }
-                if (skeleton.Joints[JointType.HandRight].TrackingState == JointTrackingState.Tracked)
+
+                if (firstSkeleton.Joints[JointType.WristRight].TrackingState ==
+                   JointTrackingState.Tracked)
                 {
-                    //this.MapJointsWithUIElement(skeleton);
+                    righthandPoint = this.MapRightJointsWithUIElement(firstSkeleton);                    
                 }
-                //if (skeleton != null && this.currentSkeletonID != skeleton.TrackingId)
-                //{
-                //    this.currentSkeletonID = skeleton.TrackingId;
-                //    int totalTrackedJoints = skeleton.Joints.Where(item => item.TrackingState == JointTrackingState.Tracked).Count();
-                //    string TrackedTime = DateTime.Now.ToString("hh:mm:ss");
-                //    string status = "Skeleton Id: " + this.currentSkeletonID + ", total tracked joints: " + totalTrackedJoints + ", TrackTime: " + TrackedTime + "\n";
-                //    this.textBlock1.Text += status;
-                //}
-                DrawSkeleton(skeleton);
-
-            }
+                if (firstSkeleton.Joints[JointType.WristLeft].TrackingState ==
+                   JointTrackingState.Tracked)
+                {
+                    lefthandPoint = this.MapLeftJointsWithUIElement(firstSkeleton);
+                }
+                                
+                if (drawObject != null)
+                {
+                    if (positionCondition == 1)
+                    {
+                        drawObject.SetValue(Canvas.LeftProperty, righthandPoint.X);
+                        drawObject.SetValue(Canvas.TopProperty, righthandPoint.Y);
+                    }
+                    else
+                    {
+                        drawObject.SetValue(Canvas.LeftProperty, lefthandPoint.X);
+                        drawObject.SetValue(Canvas.TopProperty, lefthandPoint.Y);
+                    }
+                    canvas1.Children.Add(drawObject);
+                }                
+            }            
         }
 
-        private void DrawSkeleton(Skeleton skeleton)
+        private Point MapRightJointsWithUIElement(Skeleton skeleton)
         {
-            drawBone(skeleton.Joints[JointType.Head], skeleton.Joints[JointType.ShoulderCenter]);
-            drawBone(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.Spine]);
-
-            drawBone(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.ShoulderLeft]);
-            drawBone(skeleton.Joints[JointType.ShoulderLeft], skeleton.Joints[JointType.ElbowLeft]);
-            drawBone(skeleton.Joints[JointType.ElbowLeft], skeleton.Joints[JointType.WristLeft]);
-            drawBone(skeleton.Joints[JointType.WristLeft], skeleton.Joints[JointType.HandLeft]);
-
-            drawBone(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.ShoulderRight]);
-            drawBone(skeleton.Joints[JointType.ShoulderRight], skeleton.Joints[JointType.ElbowRight]);
-            drawBone(skeleton.Joints[JointType.ElbowRight], skeleton.Joints[JointType.WristRight]);
-            drawBone(skeleton.Joints[JointType.WristRight], skeleton.Joints[JointType.HandRight]);
-
-            drawBone(skeleton.Joints[JointType.Spine], skeleton.Joints[JointType.HipCenter]);
-            drawBone(skeleton.Joints[JointType.HipCenter], skeleton.Joints[JointType.HipLeft]);
-            drawBone(skeleton.Joints[JointType.HipLeft], skeleton.Joints[JointType.KneeLeft]);
-            drawBone(skeleton.Joints[JointType.KneeLeft], skeleton.Joints[JointType.AnkleLeft]);
-            drawBone(skeleton.Joints[JointType.AnkleLeft], skeleton.Joints[JointType.FootLeft]);
-
-            drawBone(skeleton.Joints[JointType.HipCenter], skeleton.Joints[JointType.HipRight]);
-            drawBone(skeleton.Joints[JointType.HipRight], skeleton.Joints[JointType.KneeRight]);
-            drawBone(skeleton.Joints[JointType.KneeRight], skeleton.Joints[JointType.AnkleRight]);
-            drawBone(skeleton.Joints[JointType.AnkleRight], skeleton.Joints[JointType.FootRight]);
-
+            Point mappedPoint = ScalePosition(skeleton.Joints[JointType.WristRight].Position);
+            return mappedPoint;
         }
 
-        void drawBone(Joint trackedJoint1, Joint trackedJoint2)
+        private Point MapLeftJointsWithUIElement(Skeleton skeleton)
         {
-            Line bone = new Line();
-            bone.Stroke = Brushes.Red;
-            bone.StrokeThickness = 3;
-            Point joint1 = this.ScalePosition(trackedJoint1.Position);
-            bone.X1 = joint1.X;
-            bone.Y1 = joint1.Y;
-
-            Point joint2 = this.ScalePosition(trackedJoint2.Position);
-            bone.X2 = joint2.X;
-            bone.Y2 = joint2.Y;
-            //Canvas.SetLeft(bone, 200);
-            //Canvas.SetTop(bone, 5);
-            //canvas1.Children.Add(bone);
+            Point mappedPoint = ScalePosition(skeleton.Joints[JointType.WristLeft].Position);
+            return mappedPoint;
         }
-
-        //private void MapJointsWithUIElement(Skeleton skeleton)
-        //{
-        //    Point mappedPoint = ScalePosition(skeleton.Joints[JointType.HandRight].Position);
-        //    //Canvas.SetLeft(righthand, mappedPoint.X);
-        //    //Canvas.SetTop(righthand, mappedPoint.Y);
-        //}
 
         private Point ScalePosition(SkeletonPoint skeletonPoint)
         {
-            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skeletonPoint, DepthImageFormat.Resolution640x480Fps30);
+            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.
+                      MapSkeletonPointToDepthPoint(skeletonPoint, DepthImageFormat.
+                                 Resolution640x480Fps30);
             return new Point(depthPoint.X, depthPoint.Y);
-        }
+        }       
 
         private static RecognizerInfo GetKinectRecognizer()
         {
@@ -195,27 +215,29 @@ namespace Lecture12_2
             var grammarBuilder = new GrammarBuilder { Culture = recognizerInfo.Culture };
             // first say Draw
             grammarBuilder.Append(new Choices("draw"));
+
             var colorObjects = new Choices();
             colorObjects.Add("red"); colorObjects.Add("green"); colorObjects.Add("blue");
             colorObjects.Add("yellow"); colorObjects.Add("gray");
             // New Grammar builder for color
-            grammarBuilder.Append(colorObjects);
+            grammarBuilder.Append(colorObjects);            
+
             // Another Grammar Builder for object
-            grammarBuilder.Append(new Choices("circle", "square", "triangle", "rectangle"));
-
-            var jointtype = new Choices();
-            jointtype.Add("righthand"); jointtype.Add("lefthand"); jointtype.Add("rightfoot"); jointtype.Add("leftfoot");
-            grammarBuilder.Append(jointtype);
-
+            grammarBuilder.Append(new Choices("circle", "square", "rectangle"));
             // Create Grammar from GrammarBuilder
-            var grammar = new Grammar(grammarBuilder);
+            var handObject = new Choices();
+            handObject.Add("righthand","lefthand");
+            grammarBuilder.Append(handObject);
+            var grammar = new Grammar(grammarBuilder);            
 
             // Creating another Grammar and load
             var newGrammarBuilder = new GrammarBuilder();
             newGrammarBuilder.Append("close the application");
             var grammarClose = new Grammar(newGrammarBuilder);
+
             speechEngine = new SpeechRecognitionEngine(recognizerInfo.Id);
             speechEngine.LoadGrammar(grammar); // loading grammer into recognizer
+            
             speechEngine.LoadGrammar(grammarClose);
 
             // Attach the speech audio source to the recognizer
@@ -254,9 +276,7 @@ namespace Lecture12_2
 
         private void CommandsParser(SpeechRecognizedEventArgs e)
         {
-            var result = e.Result;
-            Color objectColor;
-            Shape drawObject;
+            var result = e.Result;            
             System.Collections.ObjectModel.ReadOnlyCollection<RecognizedWordUnit> words = e.Result.Words;
 
             if (words[0].Text == "draw")
@@ -282,61 +302,36 @@ namespace Lecture12_2
                     default:
                         return;
                 }
+
                 var shapeString = words[2].Text;
                 switch (shapeString)
                 {
-                    case "circle":
-                        drawObject = new Ellipse();
-                        drawObject.Width = 100; drawObject.Height = 100;
+                    case "circle":                        
+                        shapeCondition = 1;
+                        width = 30; height = 30;
                         break;
-                    case "square":
-                        drawObject = new Rectangle();
-                        drawObject.Width = 100; drawObject.Height = 100;
+                    case "square":                        
+                        shapeCondition = 2;
+                        width = 30; height = 30;
                         break;
-                    case "rectangle":
-                        drawObject = new Rectangle();
-                        drawObject.Width = 100; drawObject.Height = 60;
-                        break;
-                    case "triangle":
-                        var polygon = new Polygon();
-                        polygon.Points.Add(new Point(0, 30));
-                        polygon.Points.Add(new Point(-60, -30));
-                        polygon.Points.Add(new Point(60, -30));
-                        drawObject = polygon;
-                        break;
+                    case "rectangle":                        
+                        shapeCondition = 2;
+                        width = 40; height = 20;
+                        break;                    
                     default:
                         return;
                 }
-                var jointtype = words[3].Text;
-                switch (jointtype)
+
+                var handString = words[3].Text;
+                switch (handString)
                 {
-                    case "righthand":
-                        Point mappedPoint = new Point();
-                        if (skeleton != null)
-                        {
-                            mappedPoint = ScalePosition(skeleton.Joints[JointType.HandRight].Position);
-                        }
-                        //mappedPoint = ScalePosition(skeleton.Joints[JointType.HandRight].Position);
-                        canvas1.Children.Clear();
-                        drawObject.SetValue(Canvas.LeftProperty, mappedPoint.X - 15);
-                        drawObject.SetValue(Canvas.TopProperty, mappedPoint.Y - 15);
-                        drawObject.Fill = new SolidColorBrush(objectColor);
-                        canvas1.Children.Add(drawObject);
+                    case "righthand":                        
+                        positionCondition = 1;
                         break;
                     case "lefthand":
-                        Point mappedPoint1 = ScalePosition(skeleton.Joints[JointType.HandLeft].Position);
-                        canvas1.Children.Clear();
-                        drawObject.SetValue(Canvas.LeftProperty, mappedPoint1.X);
-                        drawObject.SetValue(Canvas.TopProperty, mappedPoint1.Y);
-                        drawObject.Fill = new SolidColorBrush(objectColor);
-                        canvas1.Children.Add(drawObject);
+                        positionCondition = 2;
                         break;
-                }
-                //canvas1.Children.Clear();
-                //drawObject.SetValue(Canvas.LeftProperty, 80.0);
-                //drawObject.SetValue(Canvas.TopProperty, 80.0);
-                //drawObject.Fill = new SolidColorBrush(objectColor);
-                //canvas1.Children.Add(drawObject);
+                }                
             }
 
             if (words[0].Text == "close" && words[1].Text == "the" && words[2].Text == "application")
